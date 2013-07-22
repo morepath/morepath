8951456c2430f104533848307f85d76ef14274b7
faassen <faassen@startifact.com>
2013-07-22 21:56:11 +0200

    Better organization of traject code.

--- morepath/traject.py
+++ morepath/traject.py
@@ -3,25 +3,15 @@ from .interfaces import ITraject, TrajectError
 from .pathstack import parse_path
 from .publish import SHORTCUTS
 
+IDENTIFIER = re.compile(r'^[^\d\W]\w*$')
+PATH_VARIABLE = re.compile(r'\{([^}]*)\}')
 VARIABLE = '{}'
 
-def normalize(pattern_str):
-    if pattern_str.startswith('/'):
-        return pattern_str[1:]
-    return pattern_str
-
-def parse(pattern_str):
-    pattern_str = normalize(pattern_str)
-    stack = parse_path(pattern_str, SHORTCUTS)
-    return tuple(reversed(stack))
-
-def subpatterns(pattern):
-    subpattern = []
-    result = []
-    for step in pattern:
-        subpattern.append(step)
-        result.append(tuple(subpattern))
-    return result
+# XXX need to make this pluggable
+KNOWN_TYPES = {
+    'str': str,
+    'int': int
+    }
 
 class Traject(object):
     def __init__(self):
@@ -71,63 +61,33 @@ class Traject(object):
         if model_factory is None:
             return None
         return model_factory(**variables)
+        
+class TrajectConsumer(object):
+    def __init__(self, lookup):
+        self.lookup = lookup
+
+    def __call__(self, base, stack):
+        traject = ITraject.component(base, lookup=self.lookup, default=None)
+        if traject is None:
+            return False, base, stack
+        variables = {}
+        pattern = ()
+        consumed = []
+        while stack:
+            step = stack.pop()
+            consumed.append(step)
+            next_pattern, matched = traject.match(pattern, step)
+            variables.update(matched)
+            if next_pattern is None:
+                break
+            pattern = next_pattern
+        model = traject.get_model(pattern, variables)
+        if model is None:
+            # put what we tried to consume back on stack
+            stack.extend(reversed(consumed))
+            return False, base, stack
+        return True, model, stack
     
-IDENTIFIER = re.compile(r'^[^\d\W]\w*$')
-PATH_VARIABLE = re.compile(r'\{([^}]*)\}')
-
-def is_identifier(s):
-    return IDENTIFIER.match(s) is not None
-
-def parse_variables(s):
-    return PATH_VARIABLE.findall(s)
-
-def create_variables_re(s):
-    return re.compile('^' + PATH_VARIABLE.sub(r'(.+)', s) + '$')
-
-
-# XXX need to make this pluggable
-KNOWN_TYPES = {
-    'str': str,
-    'int': int
-    }
-
-class NameParser(object):
-    def __init__(self, known_types):
-        self.known_types = known_types
-
-    def __call__(self, pattern, s):
-        parts = s.split(':')
-        if len(parts) > 2:
-            raise TrajectError("illegal path '%s', illegal identifier: %s" % (
-                    pattern, s))
-        if len(parts) == 1:
-            name = s.strip()
-            type_id = 'str'
-        else:
-            name, type_id = parts
-            name = name.strip()
-            type_id = type_id.strip()
-        if not is_identifier(name):
-            raise TrajectError("illegal path '%s', illegal identifier: %s" % (
-                    pattern, name))
-        converter = self.known_types.get(type_id)
-        if converter is None:
-            return TrajectError("illegal path '%s', unknown type: %s" % (
-                                pattern, type_id))
-        return name, converter
-
-def parse_variable_name(pattern, name):
-    parts = name.split(':')
-    if len(parts) == 1:
-        return name.strip(), str
-    if len(parts) != 2:
-        raise TrajectError(
-            "path '%s' cannot be parsed, illegal identifier: %s" %
-            (pattern, name))
-    name, type_id = parts
-    name = name.strip()
-    type_id = type_id.strip()
-
 class VariableMatcher(object):
     def __init__(self, step):
         self.step = step
@@ -163,30 +123,67 @@ class VariableMatcher(object):
                 # cannot convert, so isn't a match
                 return {}
         return result
-        
-class TrajectConsumer(object):
-    def __init__(self, lookup):
-        self.lookup = lookup
 
-    def __call__(self, base, stack):
-        traject = ITraject.component(base, lookup=self.lookup, default=None)
-        if traject is None:
-            return False, base, stack
-        variables = {}
-        pattern = ()
-        consumed = []
-        while stack:
-            step = stack.pop()
-            consumed.append(step)
-            next_pattern, matched = traject.match(pattern, step)
-            variables.update(matched)
-            if next_pattern is None:
-                break
-            pattern = next_pattern
-        model = traject.get_model(pattern, variables)
-        if model is None:
-            # put what we tried to consume back on stack
-            stack.extend(reversed(consumed))
-            return False, base, stack
-        return True, model, stack
+class NameParser(object):
+    def __init__(self, known_types):
+        self.known_types = known_types
 
+    def __call__(self, pattern, s):
+        parts = s.split(':')
+        if len(parts) > 2:
+            raise TrajectError("illegal path '%s', illegal identifier: %s" % (
+                    pattern, s))
+        if len(parts) == 1:
+            name = s.strip()
+            type_id = 'str'
+        else:
+            name, type_id = parts
+            name = name.strip()
+            type_id = type_id.strip()
+        if not is_identifier(name):
+            raise TrajectError("illegal path '%s', illegal identifier: %s" % (
+                    pattern, name))
+        converter = self.known_types.get(type_id)
+        if converter is None:
+            return TrajectError("illegal path '%s', unknown type: %s" % (
+                                pattern, type_id))
+        return name, converter
+
+def normalize(pattern_str):
+    if pattern_str.startswith('/'):
+        return pattern_str[1:]
+    return pattern_str
+
+def parse(pattern_str):
+    pattern_str = normalize(pattern_str)
+    stack = parse_path(pattern_str, SHORTCUTS)
+    return tuple(reversed(stack))
+
+def subpatterns(pattern):
+    subpattern = []
+    result = []
+    for step in pattern:
+        subpattern.append(step)
+        result.append(tuple(subpattern))
+    return result    
+
+def is_identifier(s):
+    return IDENTIFIER.match(s) is not None
+
+def parse_variables(s):
+    return PATH_VARIABLE.findall(s)
+
+def create_variables_re(s):
+    return re.compile('^' + PATH_VARIABLE.sub(r'(.+)', s) + '$')
+
+def parse_variable_name(pattern, name):
+    parts = name.split(':')
+    if len(parts) == 1:
+        return name.strip(), str
+    if len(parts) != 2:
+        raise TrajectError(
+            "path '%s' cannot be parsed, illegal identifier: %s" %
+            (pattern, name))
+    name, type_id = parts
+    name = name.strip()
+    type_id = type_id.strip()
