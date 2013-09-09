b9b6b17bdc574cddb944131b12d6dc5b2f2e906b
faassen <faassen@startifact.com>
2013-09-09 15:11:27 +0200

    Demonstrate an imperative test.

--- morepath/tests/test_directive.py
+++ morepath/tests/test_directive.py
@@ -4,6 +4,7 @@ from morepath.config import Config
 from morepath.request import Response
 from morepath.app import App
 from comparch import Interface
+import morepath
 
 from werkzeug.test import Client
 
@@ -102,3 +103,78 @@ def test_imperative():
 
     assert ITarget.component(lookup=app.lookup()) is foo
 
+def test_basic_imperative():
+    setup()
+
+    app = morepath.App()
+
+    class Root(object):
+        def __init__(self):
+            self.value = 'ROOT'
+
+
+    class Model(object):
+        def __init__(self, id):
+            self.id = id
+
+
+    def get_model(id):
+        return Model(id)
+
+
+    def default(request, model):
+        return "The resource for model: %s" % model.id
+
+
+    def link(request, model):
+        return request.link(model)
+
+
+    def json(request, model):
+        return {'id': model.id}
+
+
+    def root_default(request, model):
+        return "The root: %s" % model.value
+
+
+    def root_link(request, model):
+        return request.link(model)
+
+
+    c = Config()
+    c.app(app)
+    c.action(app.root(), Root)
+    c.action(app.model(model=Model, path='{id}',
+                       variables=lambda model: {'id': model.id}),
+             get_model)
+    c.action(app.resource(model=Model),
+             default)
+    c.action(app.resource(model=Model, name='link'),
+             link)
+    c.action(app.resource(model=Model, name='json',
+                          render=morepath.render_json),
+             json)
+    c.action(app.resource(model=Root),
+             root_default)
+    c.action(app.resource(model=Root, name='link'),
+             root_link)
+    c.commit()
+
+    c = Client(app, Response)
+
+    response = c.get('/foo')
+    assert response.data == 'The resource for model: foo'
+
+    response = c.get('/foo/link')
+    assert response.data == 'foo'
+
+    response = c.get('/foo/json')
+    assert response.data == '{"id": "foo"}'
+
+    response = c.get('/')
+    assert response.data == 'The root: ROOT'
+
+    # @@ is to make sure we get the view, not the sub-model
+    response = c.get('/@@link')
+    assert response.data == ''
