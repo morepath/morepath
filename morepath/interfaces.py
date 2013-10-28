5138db4353edf7414c79ca8e1e42c73b35313b15
faassen <faassen@startifact.com>
2013-10-28 16:39:19 +0100

    Remove various now unused interfaces.

--- morepath/interfaces.py
+++ morepath/interfaces.py
@@ -5,67 +5,24 @@ class Interface(object):
     __meta__ = ABCMeta
 
 
-class IConsumer(Interface):
-    """A consumer consumes steps in a stack to find an object.
-    """
+# class IConsumer(Interface):
+#     """A consumer consumes steps in a stack to find an object.
+#     """
 
-    @abstractmethod
-    def __call__(self, obj, stack, lookup):
-        """Returns a boolean meaning that some stack has been consumed,
-        an object and the rest of unconsumed stack
-        """
-
-
-class IResource(Interface):
-    pass
-
-
-class IResponse(Interface):
-    pass
-
-
-class IResponseFactory(Interface):
-    """When called, a Response instance is returned.
-    """
-    @abstractmethod
-    def __call__(self):
-        """Returns a Response instance."""
-
-
-class ITraject(Interface):
-    pass
-
-
-class IInverse(Interface):
-    """Marker interface to hook in inverse component in a traject."""
+#     @abstractmethod
+#     def __call__(self, obj, stack, lookup):
+#         """Returns a boolean meaning that some stack has been consumed,
+#         an object and the rest of unconsumed stack
+#         """
 
 
 class IRoot(Interface):
     """Mark this object as the root.
     """
 
-
-class ILookup(Interface):
-    """Mark this model as an model that can change the lookup.
-    """
-
-
 class IApp(Interface):
     """An application."""
     # XXX fill in details
-
-
-class IModelBase(Interface):
-    """Mark this object as a base of a model.
-    """
-
-
-class IPath(Interface):
-    """Get the path for a model."""
-
-
-class ILink(Interface):
-    """Get the hyperlink for a model."""
 
 
 class IConfigAction(Interface):
