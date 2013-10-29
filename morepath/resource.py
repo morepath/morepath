d18bf4c5ed8d2229ae4c4a133a0390b0657a8697
faassen <faassen@startifact.com>
2013-10-29 14:57:19 +0100

    This was dead code. Removed.

--- morepath/resource.py
+++ morepath/resource.py
@@ -4,28 +4,6 @@ from reg import PredicateRegistry, Predicate, KeyIndex, Matcher
 import json
 
 
-class PredicateLookup(object):
-    def __init__(self, predicate_registry):
-        self.predicate_registry = predicate_registry
-
-    def __call__(self, request, model):
-        return self.predicate_registry.get(self.get_predicates(request))
-
-        # XXX but wait; it could be that a matching set of predicates is
-        # registered for a base class, and now that never would be found!
-        # if some predicates match for elephant and a non-overlapping set of
-        # predicates are registered for animal, and the match is not for
-        # elephant, it should really find the match for animal if that's
-        # there, but right now it won't
-
-    # XXX move to request?
-    def get_predicates(self, request):
-        result = {}
-        result['request_method'] = request.method
-        result['name'] = request.resolver_info()['name']
-        return result
-
-
 class Resource(object):
     def __init__(self, resource, render):
         self.resource = resource
