9656b9fb1590513398bfca20f53483c6397b8095
faassen <faassen@startifact.com>
2013-10-30 14:59:42 +0100

    Raise NotImplementedError for basic stuff.

--- morepath/generic.py
+++ morepath/generic.py
@@ -5,42 +5,49 @@ import reg
 def consumer(obj):
     """A consumer consumes steps in a stack to find an object.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def app(obj):
     """Get the application that this object is associated with.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def base(model):
     """Get the base that this model is associated with.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def lookup(obj):
     """Get the lookup that this object is associated with.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def path(request, model):
     """Get the path for a model in the context of a request.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def link(request, model):
     """Create a link (URL) to model.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def traject(obj):
     """Get traject for obj.
     """
+    raise NotImplementedError
 
 
 @reg.generic
@@ -52,9 +59,11 @@ def resource(request, model):
     returned, the string is converted to a Response with the string as
     the response body.
     """
+    raise NotImplementedError
 
 
 @reg.generic
 def response(request, model):
     """Get a Response for the model in the context of the request.
     """
+    raise NotImplementedError
