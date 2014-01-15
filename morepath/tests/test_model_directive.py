0220e71b9e4aefc60e484cf0a33eb4560c212df6
faassen <faassen@startifact.com>
2014-01-15 13:40:43 +0100

    Path with two steps.

--- morepath/tests/test_model_directive.py
+++ morepath/tests/test_model_directive.py
@@ -6,7 +6,7 @@ from werkzeug.test import Client
 import pytest
 
 
-def test_simple_path():
+def test_simple_path_one_step():
     app = morepath.App()
 
     class Model(object):
@@ -36,3 +36,35 @@ def test_simple_path():
 
     response = c.get('/simple/link')
     assert response.data == '/simple'
+
+
+def test_simple_path_two_steps():
+    app = morepath.App()
+
+    class Model(object):
+        def __init__(self):
+            pass
+
+    def get_model():
+        return Model()
+
+    def default(request, model):
+        return "View"
+
+    def link(request, model):
+        return request.link(model)
+
+    c = setup()
+    c.configurable(app)
+    c.action(app.model(model=Model, path='one/two'), get_model)
+    c.action(app.view(model=Model), default)
+    c.action(app.view(model=Model, name='link'), link)
+    c.commit()
+
+    c = Client(app, Response)
+
+    response = c.get('/one/two')
+    assert response.data == 'View'
+
+    response = c.get('/one/two/link')
+    assert response.data == '/one/two'
