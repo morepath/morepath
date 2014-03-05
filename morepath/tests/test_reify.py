0638e614b729d609ba7d3d9d9c1b64b51373ebb9
faassen <faassen@startifact.com>
2014-03-05 14:21:46 +0100

    Tests for reify.

---
+++ morepath/tests/test_reify.py
@@ -0,0 +1,31 @@
+from morepath import reify
+
+# from pyramid.tests.test_decorator
+
+def test__get__with_inst():
+    def wrapped(inst):
+        return 'a'
+    decorator = reify(wrapped)
+    inst = Dummy()
+    result = decorator.__get__(inst)
+    assert result == 'a'
+    assert inst.__dict__['wrapped'] == 'a'
+
+
+def test__get__noinst():
+    decorator = reify(None)
+    result = decorator.__get__(None)
+    assert result is decorator
+
+
+def test__doc__copied():
+    def wrapped(inst):
+        """My doc"""
+
+    decorator = reify(wrapped)
+    assert decorator.__doc__ == 'My doc'
+
+
+class Dummy(object):
+    pass
+
