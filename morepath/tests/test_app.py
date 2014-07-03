c164a0e23b7378898fc7eb9a4bc2c5f0748a3882
faassen <faassen@startifact.com>
2014-07-03 12:31:26 +0200

    Remove lots of tests now irrelevant, but repair the one that tests the caching of lookup.

--- morepath/tests/test_app.py
+++ morepath/tests/test_app.py
@@ -1,4 +1,5 @@
-from morepath.app import App, global_app
+import morepath
+from morepath.app import MorepathInfo
 from morepath.error import MountError
 import morepath
 import pytest
@@ -8,69 +9,43 @@ def setup_module(module):
     morepath.disable_implicit()
 
 
-def test_global_app():
-    assert global_app.extends == []
-    assert global_app.name == 'global_app'
-
-
-def test_app_without_extends():
-    myapp = App()
-    assert myapp.extends == [global_app]
-    assert myapp.name is None
-
-
-def test_app_with_extends():
-    parentapp = App()
-    myapp = App('myapp', extends=parentapp)
-    assert myapp.extends == [parentapp]
-    assert myapp.name == 'myapp'
-
-
 def test_app_caching_lookup():
-    class MockClassLookup(object):
+    class MockMorepathInfo(MorepathInfo):
         called = 0
 
         def all(self, key, classes):
             self.called += 1
             return ["answer"]
 
-    class MockApp(MockClassLookup, App):
-        pass
+    class MockApp(morepath.App):
+        _info = None
+
+        @classmethod
+        def morepath(cls):
+            if cls._info is not None:
+                return cls._info
+            result = cls._info = MockMorepathInfo(cls, [], cls.testing_config)
+            return result
 
     myapp = MockApp()
     lookup = myapp.lookup
     answer = lookup.component('foo', [])
     assert answer == 'answer'
-    assert myapp.called == 1
+    assert myapp.morepath().called == 1
 
     # after this the answer will be cached for those parameters
     answer = lookup.component('foo', [])
-    assert myapp.called == 1
+    assert myapp.morepath().called == 1
 
     answer = myapp.lookup.component('foo', [])
-    assert myapp.called == 1
+    assert myapp.morepath().called == 1
 
     # but different parameters does trigger another call
     lookup.component('bar', [])
-    assert myapp.called == 2
-
-
-def test_app_name_repr():
-    app = morepath.App(name='foo')
-    assert repr(app) == "<morepath.App 'foo'>"
-
-
-def test_app_unnamed_repr():
-    app = morepath.App()
-    assert repr(app).startswith("<morepath.App at 0x")
+    assert myapp.morepath().called == 2
 
 
 def test_app_set_implicit():
     app = morepath.App()
     app.set_implicit()
 
-
-def test_app_mounted():
-    app = morepath.App(variables=['foo'])
-    with pytest.raises(MountError):
-        app.mounted()
