6d2ada6f47c058e5a002642cf04de69be69a43d0
faassen <faassen@startifact.com>
2014-07-28 12:28:50 +0200

    A suggestion by pylint.

--- morepath/framehack.py
+++ morepath/framehack.py
@@ -13,7 +13,7 @@ def caller_package(level=2, caller_module=caller_module):
     # caller_module in arglist for tests
     module = caller_module(level+1)
     f = getattr(module, '__file__', '')
-    if (('__init__.py' in f) or ('__init__$py' in f)):  # empty at >>>
+    if ('__init__.py' in f) or ('__init__$py' in f):  # empty at >>>
         # Module is a package
         return module
     # Go up one level to get package
