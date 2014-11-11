ce1df13b987bf16cf883eaecc5efde4d79937ed1
faassen <faassen@startifact.com>
2014-11-11 17:05:24 +0100

    Preparing release 0.7.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (
     open('CHANGES.txt').read())
 
 setup(name='morepath',
-      version='0.7.2.dev0',
+      version='0.7.2',
       description="A micro web-framework with superpowers",
       long_description=long_description,
       author="Martijn Faassen",
