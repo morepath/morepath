d78a3dfdccc27ea1505c07d9e0b6afe5f6aebdd3
faassen <faassen@startifact.com>
2014-11-11 21:50:28 +0100

    Require newer version of Reg, just released.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(name='morepath',
         'setuptools',
         'webob >= 1.3.1',
         'venusifork >= 2.0a2',
-        'reg >= 0.9'
+        'reg >= 0.9.1'
         ],
       extras_require = dict(
         test=['pytest >= 2.5.2',
