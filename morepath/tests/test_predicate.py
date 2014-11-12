b8941c019c7b06ab2be74019fe825b3cc08354ce
faassen <faassen@startifact.com>
2014-11-12 15:14:49 +0100

    Overriding predicates and fallbacks works just fine. Closes #245.

--- morepath/tests/test_predicate.py
+++ morepath/tests/test_predicate.py
@@ -249,3 +249,136 @@ def test_dispatch_external_predicates_ordering_before():
 
     assert f(Other(), '', lookup=lookup) == 'fallback'
     assert f(Other(), 'edit', lookup=lookup) == 'fallback'
+
+
+def test_dispatch_external_override_fallback():
+    config = Config()
+
+    class App(morepath.App):
+        testing_config = config
+
+    class Sub(App):
+        testing_config = config
+
+    class Foo(object):
+        pass
+
+    class Bar(object):
+        pass
+
+    class Other(object):
+        pass
+
+    @reg.dispatch_external_predicates()
+    def f(obj):
+        return "dispatch function"
+
+    @App.predicate(f, name='model', default=None, index=ClassIndex)
+    def f_obj(obj):
+        return obj.__class__
+
+    @App.predicate_fallback(f, f_obj)
+    def f_obj_fallback(obj):
+        return "f_obj_fallback"
+
+    @Sub.predicate_fallback(f, f_obj)
+    def f_obj_fallback_sub(obj):
+        return "f_obj_fallback sub"
+
+    @App.function(f, model=Foo)
+    def f_foo(obj):
+        return "foo"
+
+    @Sub.function(f, model=Foo)
+    def f_foo_sub(obj):
+        return "foo sub"
+
+    @App.function(f, model=Bar)
+    def f_bar(obj):
+        return "bar"
+
+    config.commit()
+
+    s = Sub()
+    lookup = s.lookup
+
+    assert f(Foo(), lookup=lookup) == 'foo sub'
+    assert f(Bar(), lookup=lookup) == 'bar'
+    assert f(Other(), lookup=lookup) == 'f_obj_fallback sub'
+
+    # original is unaffected
+    a = App()
+    lookup = a.lookup
+
+    assert f(Foo(), lookup=lookup) == 'foo'
+    assert f(Bar(), lookup=lookup) == 'bar'
+    assert f(Other(), lookup=lookup) == 'f_obj_fallback'
+
+
+def test_dispatch_external_override_predicate():
+    config = Config()
+
+    class App(morepath.App):
+        testing_config = config
+
+    class Sub(App):
+        testing_config = config
+
+    class Foo(object):
+        pass
+
+    class Bar(object):
+        pass
+
+    class Other(object):
+        pass
+
+    @reg.dispatch_external_predicates()
+    def f(obj):
+        return "dispatch function"
+
+    @App.predicate(f, name='model', default=None, index=ClassIndex)
+    def f_obj(obj):
+        return obj.__class__
+
+    @Sub.predicate(f, name='model', default=None, index=ClassIndex)
+    def f_obj_sub(obj):
+        return Bar # ridiculous, but lets us test this
+
+    @App.predicate_fallback(f, f_obj)
+    def f_obj_fallback(obj):
+        return "f_obj_fallback"
+
+    @App.function(f, model=Foo)
+    def f_foo(obj):
+        return "foo"
+
+    @Sub.function(f, model=Foo)
+    def f_foo_sub(obj):
+        return "foo"
+
+    @App.function(f, model=Bar)
+    def f_bar(obj):
+        return "bar"
+
+    @Sub.function(f, model=Bar)
+    def f_bar_sub(obj):
+        return "bar sub"
+
+    config.commit()
+
+    s = Sub()
+
+    lookup = s.lookup
+
+    assert f(Foo(), lookup=lookup) == 'bar sub'
+    assert f(Bar(), lookup=lookup) == 'bar sub'
+    assert f(Other(), lookup=lookup) == 'bar sub'
+
+    a = App()
+
+    lookup = a.lookup
+
+    assert f(Foo(), lookup=lookup) == 'foo'
+    assert f(Bar(), lookup=lookup) == 'bar'
+    assert f(Other(), lookup=lookup) == 'f_obj_fallback'
