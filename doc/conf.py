e70cf27ba10b7f69dc4a1865ee2f04c7f1bc17f5
faassen <faassen@startifact.com>
2016-03-29 15:09:01 +0200

    The sphinxextension now moved into dectate, use it.

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ import pkg_resources
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-              'morepath.sphinxext']
+              'dectate.sphinxext']
 
 autoclass_content = 'both'
 
