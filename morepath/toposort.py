bb178035990c6d1bc944dd908f7c5a2c95d906f5
faassen <faassen@startifact.com>
2016-04-15 12:06:28 +0200

    Start adding docstrings for this internal module.

--- morepath/toposort.py
+++ morepath/toposort.py
@@ -1,7 +1,27 @@
+"""Topological sort functionality.
+"""
+
 from .error import TopologicalSortError
 
 
 def topological_sort(l, get_depends):
+    """`Topological sort`_
+
+    .. _`Topological sort`: https://en.wikipedia.org/wiki/Topological_sorting
+
+    Given a list of items that depend on each other, sort so that
+    dependencies come before the dependent items. Dependency graph must
+    be a DAG_.
+
+    .. _DAG: https://en.wikipedia.org/wiki/Directed_acyclic_graph
+
+    :param l: a list of items to sort
+    :param get_depends: a function that given an item
+      gives other items that this item depends on. This item
+      will be sorted after the items it depends on.
+    :return: the list sorted topologically.
+
+    """
     result = []
     marked = set()
     temporary_marked = set()
@@ -19,6 +39,28 @@ def topological_sort(l, get_depends):
     for n in l:
         visit(n)
     return result
+
+
+def toposorted(infos):
+    """Sort infos topologically.
+
+    Info object must have a ``key`` attribute, and ``before`` and ``after``
+    attributes that returns a list of keys. You can use :class:`Info`.
+    """
+    key_to_info = {}
+    depends = {}
+    for info in infos:
+        key_to_info[info.key] = info
+        depends[info.key] = []
+    for info in infos:
+        for after in info.after:
+            after_info = key_to_info[after]
+            depends[info.key].append(after_info)
+        for before in info.before:
+            before_info = key_to_info[before]
+            depends[before_info.key].append(info)
+    return topological_sort(
+        infos, lambda info: depends[info.key])
 
 
 class Info(object):
@@ -41,24 +83,3 @@ def _convert_before_after(l):
     else:
         return [l]
 
-
-def toposorted(infos):
-    """Sort infos topologically.
-
-    Info object must have a key attribute, and before and after
-    attributes that returns a list of keys.
-    """
-    key_to_info = {}
-    depends = {}
-    for info in infos:
-        key_to_info[info.key] = info
-        depends[info.key] = []
-    for info in infos:
-        for after in info.after:
-            after_info = key_to_info[after]
-            depends[info.key].append(after_info)
-        for before in info.before:
-            before_info = key_to_info[before]
-            depends[before_info.key].append(info)
-    return topological_sort(
-        infos, lambda info: depends[info.key])
