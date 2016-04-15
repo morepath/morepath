9a49d0b9ebd74ae0de4ca485eaa3cd9dc3f9d0b9
faassen <faassen@startifact.com>
2016-04-15 09:01:17 +0200

    whitespace.

--- morepath/reify.py
+++ morepath/reify.py
@@ -2,7 +2,7 @@
 
 
 class reify(object):  # flake8: noqa
-    """ Use as a class method decorator.  It operates almost exactly like the
+    """Use as a class method decorator.  It operates almost exactly like the
     Python ``@property`` decorator, but it puts the result of the method it
     decorates into the instance dict after the first call, effectively
     replacing the function it decorates with an instance variable.  It is, in
