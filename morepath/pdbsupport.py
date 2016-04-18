297b5e8ec85f634c9e3b93e1773e2b634632e220
faassen <faassen@startifact.com>
2016-04-18 14:13:14 +0200

    exclude pdbsupport code from coverage.

--- morepath/pdbsupport.py
+++ morepath/pdbsupport.py
@@ -1,14 +1,15 @@
-from pdb import Pdb
+from pdb import Pdb  # pragma: nocoverage
 
 
-morepath_pdb = Pdb(skip=['reg.*', 'inspect', 'repoze.lru'])
+morepath_pdb = Pdb(
+    skip=['reg.*', 'inspect', 'repoze.lru'])  # pragma: nocoverage
 
 
-def set_trace(*args, **kw):
+def set_trace(*args, **kw):  # pragma: nocoverage
     """Set pdb trace as in ``import pdb; pdb.set_trace``, ignores ``reg``.
 
     Use ``from morepath import pdbsupport; pdbsupport.set_trace()`` to use.
 
     The debugger won't step into ``reg``, ``inspect`` or ``repoze.lru``.
     """
-    return morepath_pdb.set_trace(*args, **kw)  # pragma: nocoverage
+    return morepath_pdb.set_trace(*args, **kw)
