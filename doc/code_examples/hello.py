b97bf59337020b48bf99a42308f7002cbe11981b
faassen <faassen@startifact.com>
2016-04-25 16:08:22 +0200

    pep8.

--- doc/code_examples/hello.py
+++ doc/code_examples/hello.py
@@ -1,15 +1,19 @@
 import morepath
 
+
 class App(morepath.App):
     pass
+
 
 @App.path(path='')
 class Root(object):
     pass
 
+
 @App.view(model=Root)
 def hello_world(self, request):
     return "Hello world!"
+
 
 if __name__ == '__main__':
     morepath.autocommit()
