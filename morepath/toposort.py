395f3808de3e02b7a204c6dafa073174f2418c15
taschini <taschini@users.noreply.github.com>
2016-04-25 09:24:08 +0200

    Use topological_sort from dectate (#403)

--- morepath/toposort.py
+++ morepath/toposort.py
@@ -1,44 +1,6 @@
 """Topological sort functionality.
 """
-
-from .error import TopologicalSortError
-
-
-def topological_sort(l, get_depends):
-    """`Topological sort`_
-
-    .. _`Topological sort`: https://en.wikipedia.org/wiki/Topological_sorting
-
-    Given a list of items that depend on each other, sort so that
-    dependencies come before the dependent items. Dependency graph must
-    be a DAG_.
-
-    .. _DAG: https://en.wikipedia.org/wiki/Directed_acyclic_graph
-
-    :param l: a list of items to sort
-    :param get_depends: a function that given an item
-      gives other items that this item depends on. This item
-      will be sorted after the items it depends on.
-    :return: the list sorted topologically.
-
-    """
-    result = []
-    marked = set()
-    temporary_marked = set()
-
-    def visit(n):
-        if n in marked:
-            return
-        if n in temporary_marked:
-            raise TopologicalSortError("Not a DAG")
-        temporary_marked.add(n)
-        for m in get_depends(n):
-            visit(m)
-        marked.add(n)
-        result.append(n)
-    for n in l:
-        visit(n)
-    return result
+from dectate import topological_sort
 
 
 def toposorted(infos):
