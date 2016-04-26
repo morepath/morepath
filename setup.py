bf07f0f827945543d1e6f4f5ab57b227b0ae8bfc
faassen <faassen@startifact.com>
2016-04-26 14:25:25 +0200

    Require newer dectate.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(name='morepath',
         'setuptools',
         'webob >= 1.3.1',
         'reg >= 0.9.2',
-        'dectate >= 0.10',
+        'dectate >= 0.10.1',
         'importscan',
       ],
       extras_require = dict(
