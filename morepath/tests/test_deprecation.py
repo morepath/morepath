36932683a18e5c0a17cb0b74e2786584c3496f11
faassen <faassen@startifact.com>
2016-04-28 11:14:34 +0200

    Actually remove test_deprecation...

--- morepath/tests/test_deprecation.py
+++
@@ -1,9 +0,0 @@
-import warnings
-
-
-def test_security_to_authentication():
-    warnings.simplefilter("always")
-    with warnings.catch_warnings(record=True) as w:
-        from morepath import security  # noqa
-    assert len(w) == 1
-    assert w[0].message
