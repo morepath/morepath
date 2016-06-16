7842b40b661188fdb62fd98302d60ba5210d5bb8
Henri Hulski <henri.hulski@gazeta.pl>
2016-06-16 19:53:25 +0200

    Update setup.py and CHANGES.txt

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 5 - Production/Stable'
     ],
