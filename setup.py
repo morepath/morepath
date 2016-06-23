c0155f59f52696e178def437f712f22c610c333a
Henri Hulski <henri.hulski@gazeta.pl>
2016-06-24 00:23:06 +0200

    Add classifiers for Python 3.3 and PyPy

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 5 - Production/Stable'
     ],
     keywords="web wsgi routing morepath",
