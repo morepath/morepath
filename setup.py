2b5d462bcb23416248e1873e5c693763ad8e215a
faassen <faassen@startifact.com>
2016-06-23 13:19:23 +0200

    We need a newer py.test for the tests.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     extras_require=dict(
         test=[
-            'pytest >= 2.5.2',
+            'pytest >= 2.9.0',
             'py >= 1.4.20',
             'pytest-cov',
             'pytest-remove-stale-bytecode',
