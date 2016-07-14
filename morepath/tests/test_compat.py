078b08fcaeb567e22b92ed445d9138892b54cd50
taschini <taschini@users.noreply.github.com>
2016-07-14 09:29:07 +0200

    Added tests for the Python 2/3 compatibility module.

---
+++ morepath/tests/test_compat.py
@@ -0,0 +1,22 @@
+from morepath import compat
+
+
+def test_text_type():
+    assert isinstance(u'foo', compat.text_type)
+    assert not isinstance(b'foo', compat.text_type)
+
+
+def test_string_types():
+    assert isinstance('foo', compat.string_types)
+    assert isinstance(u'foo', compat.string_types)
+    if compat.PY3:
+        assert not isinstance(b'foo', compat.string_types)
+    else:
+        assert isinstance(b'foo', compat.string_types)
+
+
+def test_bytes_():
+    text = u'Z\N{latin small letter u with diaeresis}rich'
+    code = compat.bytes_(text)
+    assert isinstance(code, bytes)
+    assert code == compat.bytes_(code)
