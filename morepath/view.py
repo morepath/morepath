e234c5a16a47614a38f7c754e0280622142aa045
Henri Hulski <henri.hulski@gazeta.pl>
2017-03-01 14:26:33 +0100

    Fix README and inline the body_obj variable

--- morepath/view.py
+++ morepath/view.py
@@ -68,8 +68,7 @@ class View(object):
             not request.app._permits(request.identity, obj, self.permission)):
             raise HTTPForbidden()
         if self.load is not None:
-            body_obj = self.load(request)
-            content = self.func(obj, request, body_obj)
+            content = self.func(obj, request, self.load(request))
         else:
             content = self.func(obj, request)
         if isinstance(content, BaseResponse):
