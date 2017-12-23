a17b8568b418e1e9f3100c67f5e4fe72356de114
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2017-12-23 12:14:33 +0000

    Use travis-tox Remove 3.3 as it's unsupported by pytest Add 3.6 to tox Fix PEP8 violation in tests

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 5 - Production/Stable'
     ],
